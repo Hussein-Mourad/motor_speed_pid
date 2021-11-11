1b1efb9e7c5ebc18832f31a43b38e424d77814ed
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2021-11-11 13:14:30 +0200

    tweaked direction control & refactoring

--- motor_speed_pid.ino
+++ motor_speed_pid.ino
@@ -9,12 +9,14 @@ HardwareSerial Serial3(PB11, PB10);
 #define Ki 2
 #define Kd 1
 
-#define ENC_REV_COUNT 2500
+#define ENC_REV_COUNT 540
 #define ENC_A PB12
 #define ENC_B PB13
 
 #define MOTOR_PWM PB1
 #define MOTOR_DIR PB0
+
+#define INPUT_MAX_RANGE 7200
 
 #define motorDir HIGH
 
@@ -32,20 +34,20 @@ HardwareSerial Serial3(PB11, PB10);
 #define debugln(x)
 #endif
 
-const int interval = 1000; // 1000 ms
+const int interval = 10; // 10 ms
 
 volatile long long enc_counter = 0;
 
 int rpm = 0, motorPwm = 0;
 
-unsigned long previousMillis = 0, currentMillis = 0, deltaTime=0;
+unsigned long previousMillis = 0, currentMillis = 0, deltaTime = 0;
 
 int16_t setPoint = 0;
-double error,lastError, proportional, integral, derivative;
+double error, lastError, proportional, integral, derivative;
 
 void messageCb(const std_msgs::Int16 &sp)
 {
-  setPoint = sp.data;
+  setPoint = max(0, min(sp.data, INPUT_MAX_RANGE));
 }
 
 ros::NodeHandle nh;
@@ -57,8 +59,8 @@ void setup()
 
   (nh.getHardware())->setPort(&Serial1);
   (nh.getHardware())->setBaud(115200);
-  nh.initNode();
-  nh.subscribe(sub);
+  // nh.initNode();
+  // nh.subscribe(sub);
 
   pinMode(ENC_A, INPUT_PULLUP);
   pinMode(ENC_B, INPUT_PULLUP);
@@ -74,49 +76,55 @@ void setup()
 
 void loop()
 {
-  currentMillis = millis();
-  deltaTime = currentMillis - previousMillis;
-
-  if (deltaTime > interval)
-  {
-    previousMillis = currentMillis;
-
-    // Calculate RPM
-    rpm = (float)(enc_counter * MIN_IN_SECS / ENC_REV_COUNT);
-
-    // get motor pid value
-    motorPwm = calc_pid(rpm, deltaTime);
-
-    // set motor output
-    analogWrite(MOTOR_PWM, motorPwm);
-    digitalWrite(MOTOR_DIR, motorDir);
-
-    // Only update display when there is a reading
-    if (motorPwm > 0 || rpm > 0)
-    {
-      debug("PWM VALUE: ");
-      debug(motorPwm);
-      debug('\t');
-      debug(" PULSES: ");
-      debug(enc_counter);
-      debug('\t');
-      debug(" SPEED: ");
-      debug(rpm);
-      debugln(" RPM");
-    }
-    enc_counter = 0;
+  if (Serial3.available() > 0) {
+    setPoint = max(0, min(Serial3.read(), INPUT_MAX_RANGE));
   }
-  nh.spinOnce();
+
+  calc_pid();
+
+  // Only update display when there is a reading
+  if (motorPwm > 0 || rpm > 0)
+  {
+    debug("PWM VALUE: ");
+    debug(motorPwm);
+    debug('\t');
+    debug(" PULSES: ");
+    debug(enc_counter);
+    debug('\t');
+    debug(" SPEED: ");
+    debug(rpm);
+    debugln(" RPM");
+  }
+  // nh.spinOnce();
 }
 
-int calc_pid(int input, unsigned long deltaTime){
-  error = setPoint - input;
-  integral += (error * deltaTime);
-  derivative = (error - lastError) / deltaTime;
-  lastError = error;
-  int output = Kp * error + Ki * integral + Kd * derivative;
-  output = min(0,max(output, PWM_RANGE));
-  return output;
+void calc_pid() {
+  currentMillis = millis();
+  deltaTime = currentMillis - previousMillis;
+  if (deltaTime > interval) {
+
+    rpm = (enc_counter / ENC_REV_COUNT) * (MIN_IN_SECS * 1000 / deltaTime) ;
+
+    error = setPoint - rpm;
+    integral += (error * deltaTime);
+    derivative = (error - lastError) / deltaTime;
+    lastError = error;
+    int output = Kp * error + Ki * integral + Kd * derivative;
+
+    if (output < 0) {
+      digitalWrite(MOTOR_DIR, LOW);
+    } else {
+      digitalWrite(MOTOR_DIR, HIGH);
+    }
+
+    output = abs(output);
+    output = map(output, 0, INPUT_MAX_RANGE, 0, PWM_RANGE);
+
+    previousMillis = currentMillis;
+    enc_counter = 0;
+  }
+
+  analogWrite(MOTOR_PWM, OUTPUT);
 }
 
 void ISR_A()
