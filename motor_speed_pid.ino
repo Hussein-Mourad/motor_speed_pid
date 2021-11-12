9385595b09884c90d707eccb15fd244118b95139
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2021-11-12 12:59:43 +0200

    debug

--- motor_speed_pid.ino
+++ motor_speed_pid.ino
@@ -1,21 +1,21 @@
-#include <ros.h>
-#include <std_msgs/Int16.h>
+// #include <ros.h>
+// #include <std_msgs/Int16.h>
 #include "Timer.h"
 
-HardwareSerial Serial3(PB11, PB10);
+// HardwareSerial Serial(PB11, PB10);
 
 #define TOPIC_NAME "motor_pid"
 
-#define Kp 0.07
-#define Ki 0.03
-#define Kd 0.02
+#define Kp 0.5
+#define Ki 0
+#define Kd 0
 
 #define ENC_REV_COUNT 540
-#define ENC_A PA4
-#define ENC_B PA5
+#define ENC_A 4 // PA4
+#define ENC_B 5 // PA5
 
-#define MOTOR_PWM PA0
-#define MOTOR_DIR PA1
+#define MOTOR_PWM 2 //PA0
+#define MOTOR_DIR 3 //PA1
 
 #define INPUT_MAX_RANGE 300
 
@@ -26,10 +26,12 @@ HardwareSerial Serial3(PB11, PB10);
 #define DEBUG 1
 
 #if DEBUG == 1
-#define debug(x) Serial3.print(x)
-#define debugln(x) Serial3.println(x)
+#define debug(x) Serial.print(x)
+#define debugT(x, y) Serial.print(x); Serial.print(y); Serial.print("\t")
+#define debugln(x) Serial.println(x)
 #else
 #define debug(x)
+#define debugT(x, y)
 #define debugln(x)
 #endif
 
@@ -39,24 +41,26 @@ double rpm = 0;
 
 double motorPwm = 0;
 
-const int deltaTime = 10; // 10ms interval
+const int deltaTime = 50; // 10ms interval
 
 int setPoint = 0;
 double error, lastError, proportional, integral, derivative;
 
-void messageCb(const std_msgs::Int16 &sp)
-{
-  setPoint = constrain(sp.data, 0, INPUT_MAX_RANGE);
-}
+// void messageCb(const std_msgs::Int16 &sp)
+// {
+//   setPoint = constrain(sp.data, 0, INPUT_MAX_RANGE);
+// }
 
-ros::NodeHandle nh;
-ros::Subscriber<std_msgs::Int16> sub(TOPIC_NAME, &messageCb);
+// ros::NodeHandle nh;
+// ros::Subscriber<std_msgs::Int16> sub(TOPIC_NAME, &messageCb);
 
 Timer t;
 
 void setup()
 {
-  Serial3.begin(115200);
+#if DEBUG
+  Serial.begin(9600);
+#endif
 
   // (nh.getHardware())->setPort(&Serial1);
   // (nh.getHardware())->setBaud(115200);
@@ -73,15 +77,16 @@ void setup()
   pinMode(MOTOR_DIR, OUTPUT);
 
   t.every(deltaTime, calc_pid);
+
 }
 
 void loop()
 {
   // nh.spinOnce();
 
-  if (Serial3.available())
+  if (Serial.available())
   {
-    int input = Serial3.readString().toInt();
+    int input = Serial.readString().toInt();
     setPoint = constrain(input, -1 * INPUT_MAX_RANGE, INPUT_MAX_RANGE);
   }
 
@@ -91,6 +96,8 @@ void loop()
 void calc_pid()
 {
   int deltaCount = enc_count - last_count;
+  
+
   rpm = (((float)deltaCount / ENC_REV_COUNT)) * (MIN_IN_SECS * 1000.0 / deltaTime);
 
   error = setPoint - rpm;
@@ -106,29 +113,21 @@ void calc_pid()
 
   motorPwm = abs(motorPwm);
   motorPwm = constrain(motorPwm, 0, PWM_MAX_RANGE);
-//  motorPwm = constrain(motorPwm, 0, INPUT_MAX_RANGE);
-//  motorPwm = map(motorPwm, 0, INPUT_MAX_RANGE, 0, PWM_MAX_RANGE);
-
-  debug("PWM: ");
-  debug(motorPwm);
-  debug('\t');
-
-  debug("Count: ");
-  debug(enc_count);
-  debug('\t');
-
-  debug(" SPEED: ");
-  debug(rpm);
-  debug('\t');
-  
-  debug("SP: ");
-  debug(setPoint);
-  debugln(" ");
+  //  motorPwm = constrain(motorPwm, 0, INPUT_MAX_RANGE);
+  //  motorPwm = map(motorPwm, 0, INPUT_MAX_RANGE, 0, PWM_MAX_RANGE);
+  debugT("deltaCount: ", deltaCount);
+  debugT("error: ", error);
+  debugT("PWM: ", motorPwm);
+  debugT("Count: ", enc_count);
+  debugT("Speed: ", rpm);
+  debugT("SP: ", setPoint);
+  debugln();
 
   last_count = enc_count;
 
   analogWrite(MOTOR_PWM, motorPwm);
 }
+
 
 void ISR_A()
 {
