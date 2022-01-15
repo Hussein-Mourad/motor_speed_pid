cab0bd7fc64ac8cb1202cbcf7e759b7de0563513
Hussein-Mourad <38833034+Hussein-Mourad@users.noreply.github.com>
2022-01-15 23:56:42 +0200

    code split

--- Config.h
+++ Config.h
@@ -1,1 +1,33 @@
-#define X 5
+#define TOPIC_NAME "motor_pid"
+
+#define Kp 3.0
+#define Ki 0
+#define Kd 0
+#define Ti 60
+
+#define ENC_A PB3  // PA4
+#define ENC_B PA15 // PA5
+
+#define MOTOR_PWM PB0 // PA0
+#define MOTOR_DIR PB1 // PA1
+
+#define ENC_REV_COUNT 540
+#define INPUT_MAX_RANGE 600
+#define PWM_MAX_RANGE 65535
+#define MIN_IN_MS 60000 // one minute in milli seconds
+
+// A simple way to toggle debugging and changing the used serial
+#define DEBUG
+
+#ifdef DEBUG
+#define debug(x) Serial3.print(x)
+#define debugT(x, y) \
+  Serial3.print(x);  \
+  Serial3.print(y);  \
+  Serial3.print(",  ");
+#define debugln(x) Serial3.println(x)
+#else
+#define debug(x)
+#define debugT(x, y)
+#define debugln(x)
+#endifm
--- motor_speed_pid.ino
+++ motor_speed_pid.ino
@@ -1,44 +1,10 @@
 // #include <ros.h>
 // #include <std_msgs/Int16.h>
 #include <Timer.h>
-#include "Configuration.h"
-
+#include "Config.h"
 
 HardwareSerial Serial3(PB11, PB10);
 
-#define TOPIC_NAME "motor_pid"
-
-#define Kp 3.0
-#define Ki 0
-#define Kd 0
-#define Ti 60
-
-#define ENC_A PB3  // PA4
-#define ENC_B PA15 // PA5
-
-#define MOTOR_PWM PB0 // PA0
-#define MOTOR_DIR PB1 // PA1
-
-#define ENC_REV_COUNT 540
-#define INPUT_MAX_RANGE 600
-#define PWM_MAX_RANGE 65535
-#define MIN_IN_MS 60000 // one minute in milli seconds
-
-// A simple way to toggle debugging and changing the used serial
-#define DEBUG
-
-#ifdef DEBUG
-#define debug(x) Serial3.print(x)
-#define debugT(x, y) \
-  Serial3.print(x);  \
-  Serial3.print(y);  \
-  Serial3.print(",  ");
-#define debugln(x) Serial3.println(x)
-#else
-#define debug(x)
-#define debugT(x, y)
-#define debugln(x)
-#endif
 
 volatile long encoderCount = 0, lastCount = 0;
 
@@ -72,7 +38,6 @@ void setup()
   Serial3.begin(115200);
 #endif
 
-debug(X);
 
   // (nh.getHardware())->setPort(&Serial1);
   // (nh.getHardware())->setBaud(115200);
@@ -126,7 +91,7 @@ void calc_pid()
   derivative = (error - lastError) / deltaTime;
   lastError = error;
   float Kc = 1 / Kp;
-  // motorPwm = Kc * error + (Kc/Ti) * integral + Kd * derivative;
+//   motorPwm = Kc * error + (Kc/Ti) * integral + Kd * derivative;
   motorPwm = Kc * error;
 
   if (motorPwm < 0)
